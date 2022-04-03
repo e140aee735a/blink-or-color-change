2fc78cd1118dee8467a5c8373ebc71c2f706678c
e140aee735a <79771392+e140aee735a@users.noreply.github.com>
2022-04-02 21:35:09 -0400

    now we reset the colour to green after 2s

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from m5ui import *
 from uiflow import *
 import time
 
-setScreenColor(0x84b71e)
+setScreenColor(0x8cb931)
 
 
 
@@ -17,6 +17,8 @@ while True:
     setScreenColor(0xc05db1)
     wait(2)
     setScreenColor(0x0d2563)
+    wait(2)
+    lcd.fill(0x009900)
   else:
     for count in range(3):
       M5Led.on()
